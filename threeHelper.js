eacb009c44563a6457908d9cb5e5ee1ae148cc8d
Jasmine Otto <15673619+JazzTap@users.noreply.github.com>
2024-05-03 13:08:39 -0700

    correctly populate all seven keys in the parallel coordinates plot

--- threeHelper.js
+++ threeHelper.js
@@ -27,12 +27,12 @@ export function makeScene(elem) {
     elem.style.width = width
     elem.style.height = height
     
-		const camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 ); // cleaner! wants a 2x2 plane
-    // const camera = new THREE.PerspectiveCamera(fov, aspect, .1, 5);
-    // camera.position.z = .6; // by trial and error
+		const camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 ); // wants a 2x2 plane
+    camera.position.set(0, 0, 1);
+    // camera.lookAt(0, 0, 0)
 
-    const geometry = new THREE.PlaneGeometry( 1, 1 );
-    geometry.attributes.position.set(0,0,2);
+    // used to debug camera, could replace this with a scene background 
+    const geometry = new THREE.PlaneGeometry( 2, 2 );
     const material = new THREE.MeshBasicMaterial( {color: 0xaaaaaa, transparent: true, opacity: 0.1 } )// , side: THREE.DoubleSide} ); 
     const backing = new THREE.Mesh(geometry, material);
     scene.add(backing)
